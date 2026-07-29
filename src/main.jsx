2efbc86d14a34c27ecf17635d740c4cdf751e792
Sardar balnour <balnour.sardar@gmail.com>
2026-07-29 15:41:58 +0330

    store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
 
+import store from "./app/store.js";
 import "./global.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
